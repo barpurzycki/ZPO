5320769e772d7f7f1456b879b30bad11af6de2e0
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2025-02-25 16:03:58 +0100

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,81 @@
+from collections import namedtuple
+from dataclasses import dataclass, field
+
+#Zadanie 1.
+class Employee:
+    first_name: str
+    last_name: str
+    salary: float
+
+    def __init__(self, first_name: str, last_name: str, salary: float) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self) -> str:
+        return f"Nazywam się {self.first_name} {self.last_name}. Zarabiam {self.salary}."
+
+class Manager(Employee):
+    department: str
+
+    def __init__(self, first_name: str, last_name: str, salary: float, department: str) -> None:
+        super().__init__(first_name, last_name, salary)
+        self.department = department
+
+    def get_department_info(self) -> str:
+        return f"Zarządzam działem {self.department}."
+
+
+pracownik = Employee("Bartosz", "Purzycki", 10000)
+
+print(pracownik.get_full_name())
+
+manager = Manager("Jan", "Kowalski", 15000, "IT")
+
+print(manager.get_full_name())
+print(manager.get_department_info())
+
+#Zadanie 2.
+
+Transaction = namedtuple("Transaction", ["transaction_id", "amount", "currency"])
+
+class BankAccount:
+    balance: int
+
+    def __init__(self, balance: int) -> None:
+        self.balance = balance
+
+    def apply_transaction(self, Transaction: Transaction):
+        self.balance += Transaction.amount
+
+Account = BankAccount(10000)
+Transaction = Transaction(1, 1000, 1)
+
+Account.apply_transaction(Transaction)
+print(Account.balance)
+
+#Zadanie 3.
+
+# Napisać klasę Book używając dataclass,
+# która zawiera title, author, year, price.
+# Dodaj metodę apply_discount(),
+# która obniży cenę książki o podany procent.
+
+@dataclass(frozen=True)
+class Book:
+    title: str
+    author: str
+    year: int
+    price: float
+
+    def apply_discount(self, discount: int):
+        return self.price - (self.price * (discount / 100))
+
+Book = Book("Pani Jeziora", "Andrzej Sapkowski", 1999, 60)
+print(Book.price)
+print(Book.apply_discount(50))
+
+#Zadanie 4.
+
+
+
