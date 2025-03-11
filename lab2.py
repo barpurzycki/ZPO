7f5596c8593d652f73d9e8be7e2d8d132afa35ed
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2025-03-11 16:15:43 +0100

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -98,6 +98,10 @@ meat_pizza = director.construct_meat(meat_builder)
 
 print(meat_pizza)
 
+#Zadanie 2.
+
+#Klasa Computer
+
 class Computer:
   RAM: str
   CPU: str
@@ -111,6 +115,8 @@ class Computer:
     self.GPU = GPU
     self.Motherboard = Motherboard
     self.Charger = Charger
+
+#Klasa Computer - Builder
 
 class Computer:
     def __init__(self) -> None:
@@ -156,33 +162,33 @@ class Director:
         builder.add_charger()
         return builder.get_computer()
 
-#Zadanie 2.
+#Zadanie 2. A
 
 class Document(ABC):
-  @abstractmethod
-  def get_type(self) -> str:
-    pass
+    @abstractmethod
+    def get_type(self) -> str:
+        pass
 
 class DocumentFactory(ABC):
-  @abstractmethod
-  def create_doc(self) -> Document:
-    pass
+    @abstractmethod
+    def create_doc(self) -> Document:
+        pass
 
 class WordDocument(Document):
-  def get_type(self) -> str:
-    return "doc"
+    def get_type(self) -> str:
+        return "doc"
 
 class PDFDocument(Document):
-  def get_type(self) -> str:
-    return "pdf"
+    def get_type(self) -> str:
+        return "pdf"
 
 class WordDocumentFactory(DocumentFactory):
-  def create_doc(self) -> Document:
-    return WordDocument()
+    def create_doc(self) -> Document:
+        return WordDocument()
 
 class PDFDocumentFactory(DocumentFactory):
-  def create_doc(self) -> Document:
-    return PDFDocument()
+    def create_doc(self) -> Document:
+        return PDFDocument()
 
 word_factory = WordDocumentFactory()
 pdf_factory = PDFDocumentFactory()
@@ -192,3 +198,38 @@ pdf = pdf_factory.create_doc()
 
 print(word.get_type())
 print(pdf.get_type())
+
+#Zadanie 2. B
+
+class Animal(ABC):
+
+    @abstractmethod
+    def get_type(self) -> str:
+        pass
+
+class Cat(Animal):
+    def get_type(self) -> str:
+        return "Cat"
+
+class Dog(Animal):
+    def get_type(self) -> str:
+        return "Dog"
+
+class AnimalFactory:
+
+    def create_animal(animal) -> Animal:
+        if animal == "Cat":
+            return Cat()
+        if animal == "Dog":
+            return Dog()
+        else:
+            raise ValueError(animal)
+
+animal = AnimalFactory.create_animal("Cat")
+print(animal.get_type())
+
+animal2 = AnimalFactory.create_animal("Dog")
+print(animal2.get_type())
+
+animal3 = AnimalFactory.create_animal("Parrot")
+print(animal3.get_type())
